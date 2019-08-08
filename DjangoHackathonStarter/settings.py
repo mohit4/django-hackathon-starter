8c2d176e4ebf83c95b86160a4697042f555bfa63
mohit4 <mohitkumar2801@gmail.com>
2019-08-09 00:14:16 +0530

    Configured crispy forms

--- DjangoHackathonStarter/settings.py
+++ DjangoHackathonStarter/settings.py
@@ -40,7 +40,10 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'app',
     'account',
+    'crispy_forms',
 ]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
