a0af2e969455138b450c1c538a1d8c04c4c5604d
mohit4 <mohitkumar2801@gmail.com>
2019-08-10 21:34:16 +0530

    Added user register view and handled front end for the same

--- account/urls.py
+++ account/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
 
+from .views import UserRegisterView
+
 app_name = 'account'
 
 urlpatterns = [
     path('', LoginView.as_view(template_name='account/login.html'), name='login'),
-    path('logout/', LogoutView.as_view(), name='logout')
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('register/', UserRegisterView.as_view(), name='register'),
 ]
--- account/views.py
+++ account/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.views.generic import CreateView
+from django.urls import reverse_lazy
 
 # Create your views here.
+class UserRegisterView(CreateView):
+    '''
+    New user register view
+    '''
+    form_class = UserCreationForm
+    success_message = 'User registered successfully! Please proceed to login.'
+    success_url = reverse_lazy('account:login')
+    template_name = 'account/register.html'
