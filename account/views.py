7b36ec737bf325833f924f648bf5671cef223587
mohit4 <mohitkumar2801@gmail.com>
2019-08-11 21:01:24 +0530

    Updated user creation form

--- account/views.py
+++ account/views.py
@@ -1,14 +1,15 @@
 from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
 from django.views.generic import CreateView
 from django.urls import reverse_lazy
+
+from .forms import UserRegisterForm
 
 # Create your views here.
 class UserRegisterView(CreateView):
     '''
     New user register view
     '''
-    form_class = UserCreationForm
+    form_class = UserRegisterForm
     success_message = 'User registered successfully! Please proceed to login.'
     success_url = reverse_lazy('account:login')
     template_name = 'account/register.html'
