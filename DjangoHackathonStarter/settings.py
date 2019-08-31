b8a9b78e8788768ed16814b7a5998e79882744f8
mohit4 <mohitkumar2801@gmail.com>
2019-08-31 15:39:20 +0530

    Added static root configurations

--- DjangoHackathonStarter/settings.py
+++ DjangoHackathonStarter/settings.py
@@ -131,6 +131,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 # Parameters for static file handling
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
