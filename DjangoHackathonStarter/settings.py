0d0e191e06ef292a230af07f33d6652cd141bc76
mohit4 <mohitkumar2801@gmail.com>
2019-08-31 15:47:31 +0530

    Static root fix

--- DjangoHackathonStarter/settings.py
+++ DjangoHackathonStarter/settings.py
@@ -131,7 +131,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 # Parameters for static file handling
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
