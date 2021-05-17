2549bc52d30750ed4af89c8a45b7945a34cd030f
mohit4 <mohitkumar2801@gmail.com>
2021-05-17 23:34:33 +0530

    Added user registration and authentication

--- app/accounts/views.py
+++ app/accounts/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.contrib.messages.views import SuccessMessageMixin
+from django.contrib.auth.views import LoginView
 
-# Create your views here.
+from .forms import RegistrationForm
+
+
+class SignUpView(SuccessMessageMixin, CreateView):
+    """
+    Register view
+    """
+    form_class = RegistrationForm
+    success_url = reverse_lazy('login')
+    template_name = 'registration/signup.html'
+    success_message = 'Registeration completed successfully! Please login to continue.'
+
+
+class LoginFormView(SuccessMessageMixin, LoginView):
+    """
+    View for logging in to the system
+    """
+    template_name = 'registration/login.html'
+    success_url = '/'
+    success_message = 'Logged in successfully!'
--- app/dhs/settings.py
+++ app/dhs/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
     'core',
     'accounts',
 ]
@@ -56,7 +57,9 @@ ROOT_URLCONF = 'dhs.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'templates',
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
