bf500a602bf4c46a3207f320e52b10929f7cff27
mohit4 <mohitkumar2801@gmail.com>
2021-05-18 22:58:14 +0530

    Meteor

--- app/accounts/forms.py
+++
@@ -1,8 +0,0 @@
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
-
-
-class RegistrationForm(UserCreationForm):
-    class Meta:
-        model = User
-        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2')
